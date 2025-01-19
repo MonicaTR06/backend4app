a532800797fbf3d62f4e679e9a1c8a287c9cc5f6
Monica <yuranim06@gmail.com>
2025-01-18 19:50:59 -0500

    feat : create user

--- src/main/kotlin/com/example/backend4frontend/service/user/UserAppServiceImpl.kt
+++ src/main/kotlin/com/example/backend4frontend/service/user/UserAppServiceImpl.kt
@@ -1,5 +1,7 @@
 package com.example.backend4frontend.service.user
 
+import com.example.backend4frontend.data.domain.entity.UserApp
+import com.example.backend4frontend.data.dto.user.UserCreateRequest
 import com.example.backend4frontend.data.dto.user.UserFetchResponse
 import com.example.backend4frontend.data.dto.user.UserRequest
 import com.example.backend4frontend.repository.UserRepository
@@ -13,7 +15,14 @@ class UserAppServiceImpl(
 ) : UserAppService {
 
     override fun findUserByUsernameAndPassword(userRequest: UserRequest): UserFetchResponse {
-        val user = repository.findUserByUsernameAndPassword(userRequest.username, userRequest.password)
+        val user =
+            repository.findUserByUsernameAndPassword(userRequest.username, userRequest.password)
         return mapper.toDto(user)
+    }
+
+    override fun createUserApp(createRequest: UserCreateRequest): UserFetchResponse {
+        val user: UserApp = mapper.toEntity(createRequest)
+        val savedTask: UserApp = repository.save(user)
+        return mapper.toDto(savedTask)
     }
 }
